60de86b47df5e416204475d82b888c20830b5973
AlexeyIvashchenko <iwashenko-lexa@mail.ru>
2023-08-16 16:53:59 +0300

    upload the files

--- src/item.py
+++ src/item.py
@@ -9,9 +9,12 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         return self.price * self.pay_rate
 
     def apply_discount(self) -> None:
         self.price = self.calculate_total_price()
+
+
